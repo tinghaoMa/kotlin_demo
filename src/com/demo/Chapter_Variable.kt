3b0d062f008bdbe92deb4cd9f4ee3658bd56af61
tinghaoma <tinghaoma@sohu-inc.com>
2018-05-11 10:14:30 +0800

    变量练习

---
+++ src/com/demo/Chapter_Variable.kt
@@ -0,0 +1,23 @@
+/**
+ *  变量
+ *
+ * @author
+ *
+ */
+package com.demo
+
+fun main(args: Array<String>) {
+    val a: Int = 1  // 立即赋值
+    val b = 2   // 自动推断出 `Int` 类型
+    val c: Int  // 如果没有初始值类型不能省略
+    c = 3       // 明确赋值
+    println("a = $a, b = $b, c = $c")
+
+
+    
+
+
+
+
+
+}
