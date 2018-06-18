7650d54b02ad3e2eeff43ed399abd2d6bd5151e5
tinghaoma <tinghaoma@sohu-inc.com>
2018-06-18 17:01:38 +0800

    构造函数字段前面增加var 或者 val 修饰

--- src/com/demo/obj/Chapter_Constructor.kt
+++ src/com/demo/obj/Chapter_Constructor.kt
@@ -11,20 +11,29 @@ fun main(args: Array<String>) {
     println(p.name)
     println(p.age)
 
-}
-
-class Person(name:String,age:Int){ //创建的时候就需要修改name和age
-
-    var name=""
-    var age=0
-    init {  //构造函数里面的代码可以放到init里面执行
-        this.name=name
-        this.age = age
-    }
 
 }
 
+/**
+ * 直接在构造字段前面加 var或者val
+ *
+ * 如果是val的话 相当于隐藏该属性的set方法,不能赋值
+ */
+class Person(val name: String, var age: Int) {
 
+}
+
+
+//class Person(name:String,age:Int){ //创建的时候就需要修改name和age
+//
+//    var name=""
+//    var age=0
+//    init {  //构造函数里面的代码可以放到init里面执行
+//        this.name=name
+//        this.age = age
+//    }
+//
+//}
 
 
 //class Person{
