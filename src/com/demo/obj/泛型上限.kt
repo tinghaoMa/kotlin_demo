ad8fee4d7d6d90d4affabdcde773020293baa892
tinghaoma <tinghaoma@sohu-inc.com>
2018-06-18 20:28:34 +0800

    泛型上限

--- src/com/demo/obj/泛型上限.kt
+++ src/com/demo/obj/泛型上限.kt
@@ -1,2 +1,39 @@
-package com.demo.obj
+package com.demo.obj.c
+
+/**
+ * 泛型上限
+ *
+ *
+ * T : Fruit 泛型上限 泛型只能是Fruit或者是其子类
+ */
+fun main(args: Array<String>) {
+    val thing = Thing()
+    val apple = Apple()
+
+    val fruit = Fruit()
+
+    val box = FruitBox(apple)
+    val box2 = FruitBox(fruit)
+
+//    val box1 = FruitBox(thing)
+
+
+}
+
+
+open class Box<T>(var thing: T)
+
+
+//水果箱子
+class FruitBox<T : Fruit>(thing: T) : Box<T>(thing)
+
+
+open class Thing
+
+//水果
+open class Fruit : Thing()
+
+class Apple : Fruit()
+
+class Oriange : Fruit()
 
