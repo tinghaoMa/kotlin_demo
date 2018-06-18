0935d49f91766f5a7be725c39cfdef87ea9fae43
tinghaoma <tinghaoma@sohu-inc.com>
2018-06-18 18:27:06 +0800

    java接口函数不准有方法体，但是kotlin可以有 子类不用复写该方法

--- src/com/demo/obj/接口.kt
+++ src/com/demo/obj/接口.kt
@@ -9,9 +9,9 @@ fun main(args: Array<String>) {
 class XiaoMing : ZHHuman(), RideBike, DriveCar {
     override var license: String = "123123123"
 
-    override fun driveCar() {
-        println("小明会开汽车 他的驾照号码是 ： " + this.license)
-    }
+//    override fun driveCar() {
+//        println("小明会开汽车 他的驾照号码是 ： " + this.license)
+//    }
 
     override fun ride() {
         println("小明会骑自行车")
@@ -30,6 +30,10 @@ interface RideBike {
 interface DriveCar {
 
     var license: String //不能直接初始化 但是java可以
-
-    fun driveCar()
+    /**
+     * java 接口里面函数不准有方法体 但是kotlin可以有，子类可以不复写该方法
+     */
+    fun driveCar() {
+        println("挂挡 踩油门 出发 驾照号码是 :" + this.license)
+    }
 }
