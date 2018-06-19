af72ade9e11589b5b83e80d4dad70bbcdbb67748
tinghaoma <tinghaoma@sohu-inc.com>
2018-06-19 23:30:32 +0800

    let表达式

--- src/com/demo/study5/22.四大函数.kt
+++ src/com/demo/study5/22.四大函数.kt
@@ -17,16 +17,36 @@ fun main(args: Array<String>) {
     set {
         println(name)
     }
-
-    println("/************************apply************************/")
+    /******************************apply******************************/
     //跟上面对比这种方式更好一些
+    /**
+     * 任意类型都有apply函数
+     * apply参数是一个函数 T.()->Unit 带接受者的函数字面值
+     * lambda表达式里面的this代表调用的对象
+     * 在lambda表达式里面可以访问对象的方法和变量
+     */
     val apply = list?.apply {
         add("王者荣耀")
         add("桌球")
         add("坦克大战僵尸")
     }
 
-    apply?.forEach {
+//    apply?.forEach {
+//        println(it)
+//    }
+    /*******************************let*******************************/
+    /**
+     * 任意对象都有let扩展函数
+     * let函数参数也是一个函数
+     * 函数参数他的参数是调用者本身
+     * let函数返回的值是参数函数的返回值 就是lambda表达式的返回值
+     */
+    list?.let {
+        it.add("变形金刚")
+        it.add("侏罗纪公园")
+    }
+
+    list?.forEach {
         println(it)
     }
 }
