f8f9122286d28be0a5649ec9a8bd3fa39055661b
tinghaoma <tinghaoma@sohu-inc.com>
2018-06-19 11:27:30 +0800

    object的单例

---
+++ src/com/demo/study4/09.Kotlin的object单例.kt
@@ -0,0 +1,29 @@
+package com.demo.study4
+
+/**
+ * @class name：com.demo.study4
+ * @author itck_mth
+ * @time 2018/6/19 11:20 AM
+ * @class describe
+ *
+ * Object 所有的字段都是静态的  方法不是
+ * 使用条件：字段不多的时候使用
+ */
+
+fun main(args: Array<String>) {
+    val utils = Utils
+
+    println(utils.name)
+    utils.sayHello()
+}
+
+/**
+ * 设置成一个单例
+ */
+object Utils {
+    var name = "张三"  //static
+
+    fun sayHello() {
+        println("Utils.sayHello")
+    }
+}
